fdfddc8d77086cc776b7653c3ef069299f199d4a
TakWolf <takwolf@foxmail.com>
2024-09-01 05:05:34 +0800

    升级构建

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("com.github.TakWolf.Android-InsetsWidget:insetswidget:0.0.1")
     implementation("com.github.TakWolf:Android-AspectRatioLayout:0.0.1")
-    implementation("io.coil-kt:coil:2.6.0")
+    implementation("io.coil-kt:coil:2.7.0")
     implementation(project(":hfrecyclerview"))
     implementation(project(":paging"))
 }

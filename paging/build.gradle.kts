5d88f4503179c496cbe656403d8565d4d53a5cd8
TakWolf <takwolf@foxmail.com>
2024-09-14 15:13:45 +0800

    0.0.14

--- paging/build.gradle.kts
+++ paging/build.gradle.kts
@@ -43,7 +43,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.takwolf.android.hfrecyclerview"
             artifactId = "paging"
-            version = "0.0.13"
+            version = "0.0.14"
 
             afterEvaluate {
                 from(components["release"])

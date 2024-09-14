2eda7e70a4688530691cb69ccd57f5cf8eea5220
TakWolf <takwolf@foxmail.com>
2024-09-14 21:30:23 +0800

    添加 RefreshSource

---
+++ paging/src/main/java/com/takwolf/android/hfrecyclerview/paging/RefreshSource.kt
@@ -0,0 +1,46 @@
+package com.takwolf.android.hfrecyclerview.paging
+
+import kotlinx.coroutines.flow.MutableStateFlow
+
+abstract class RefreshSource {
+    val state = MutableStateFlow(RefreshState.IDLE)
+
+    protected var dataVersion = 0
+        private set
+
+    protected fun checkDataVersion(dataVersion: Int): Boolean {
+        return this.dataVersion == dataVersion
+    }
+
+    protected fun invalidDataVersion() {
+        dataVersion += 1
+    }
+
+    fun refresh() {
+        if (state.value.canRefresh) {
+            state.value = RefreshState.LOADING
+            doRefresh(dataVersion)
+        }
+    }
+
+    protected abstract fun doRefresh(dataVersion: Int)
+
+    protected fun onRefreshSuccess(dataVersion: Int): Boolean {
+        if (checkDataVersion(dataVersion)) {
+            invalidDataVersion()
+            state.value = RefreshState.IDLE
+            return true
+        } else {
+            return false
+        }
+    }
+
+    protected fun onRefreshFailure(dataVersion: Int): Boolean {
+        if (checkDataVersion(dataVersion)) {
+            state.value = RefreshState.FAILED
+            return true
+        } else {
+            return false
+        }
+    }
+}

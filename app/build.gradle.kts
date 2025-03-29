7e992a73275deccac8c2d70fff0051d5b0d56a62
TakWolf <takwolf@foxmail.com>
2025-03-30 05:02:54 +0800

    升级 SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.takwolf.android.demo.hfrecyclerview"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.takwolf.android.demo.hfrecyclerview"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.0.1"
     }
--- hfrecyclerview/build.gradle.kts
+++ hfrecyclerview/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.takwolf.android.hfrecyclerview"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 21

8fd1c4bf3726d873f8be1a9a481616351b3f9644
TakWolf <takwolf@foxmail.com>
2025-12-06 04:42:51 +0800

    升级构建

--- paging/build.gradle.kts
+++ paging/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
     id("com.vanniktech.maven.publish")
 }
 
@@ -20,17 +19,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     compileOnly("androidx.lifecycle:lifecycle-common:2.10.0")
     compileOnly("androidx.recyclerview:recyclerview:1.4.0")
-    compileOnly("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    compileOnly("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0")
     api(project(":hfrecyclerview"))
 }
 
